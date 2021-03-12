dbc5a8e7c6ebbb5fd4929337d20bea6d1588f2f0
Amurdock6 <77793000+Amurdock6@users.noreply.github.com>
2021-03-12 12:34:05 -0700

    rev5

---
+++ 2021/js/main.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', () => {
+    const preload = document.querySelector('.preload');
+    setTimeout( function() {
+        preload.classList.add('preload-finish');
+    }, 0 );
+});
